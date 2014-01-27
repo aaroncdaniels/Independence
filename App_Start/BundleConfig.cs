2ba37ee3da2b226fc4d3345a1cd4a39511edd76c
Aaron Daniels <aaroncdaniels@gmail.com>
2014-01-26 21:56:06 -0600

    Initial check-in

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Independence
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts")
+                .Include("~/Scripts/jquery-1.9.1.js")
+                .Include("~/Scripts/bootstrap.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr")
+                .Include("~/Scripts/modernizr-2.7.1.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/bootstrap.css")
+                .Include("~/Content/site.css")
+                .Include("~/Content/site-design.css"));
+        }
+    }
+}
